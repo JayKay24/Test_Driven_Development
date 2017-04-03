35378c5f10301761bcdb579eaf68b005e3db6bb2
James Kinyua <jameskinyua590@gmail.com>
2017-04-04 00:54:06 +0300

    Add test case test_it_returns_new_loan_as_a_float

--- bank_interest.py
+++ bank_interest.py
@@ -1,4 +1,4 @@
-def calculate_loan_amount(amount=100000, time=12, rate=12):
+def calculate_loan_amount(amount=100000, time=12, rate=12.0):
     """
     Accepts the values of amount, time and rate.
     Calculates the value of loan using the values.
@@ -8,6 +8,8 @@ def calculate_loan_amount(amount=100000, time=12, rate=12):
         return "Time cannot be less than 1."
     elif amount < 100000:
         return "Amount cannot be less than 100000."
+    elif not isinstance(rate, float):
+        return "Rate should be a decimal number."
     
     new_loan_amt = (amount * (rate/100) * (time/12)) + amount
     return new_loan_amt
--- bank_interest_test.py
+++ bank_interest_test.py
@@ -12,5 +12,11 @@ class testCalculateLoanAmount(unittest.TestCase):
     def test_amount_is_not_less_than_100000(self):
         self.assertEqual(calculate_loan_amount(amount=90000), "Amount cannot be less than 100000.")
 
+    def test_rate_is_a_float(self):
+        self.assertEqual(calculate_loan_amount(rate="12.2"), "Rate should be a decimal number.")
+
+    def test_it_returns_new_loan_as_a_float(self):
+        self.assertTrue(isinstance(calculate_loan_amount(), float), "New amount should be returned as a decimal.")
+
 if __name__ == "__main__":
     unittest.main()
