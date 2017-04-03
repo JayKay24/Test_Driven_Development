7533c649f1ec922091c38bb3328bb572da444f07
James Kinyua <jameskinyua590@gmail.com>
2017-04-04 00:41:55 +0300

    Update self.assertEquals to self.assertEqual

--- bank_interest_test.py
+++ bank_interest_test.py
@@ -4,10 +4,10 @@ from bank_interest import calculate_loan_amount
 
 class testCalculateLoanAmount(unittest.TestCase):
     def test_it_calculates_correct_amount(self):
-        self.assertEquals(calculate_loan_amount(), 112000.00)
+        self.assertEqual(calculate_loan_amount(), 112000.00)
 
     def test_time_is_not_less_than_one(self):
-        self.assertEquals(calculate_loan_amount(time=-1), "Time cannot be less than 1.")
+        self.assertEqual(calculate_loan_amount(time=-1), "Time cannot be less than 1.")
 
 if __name__ == "__main__":
     unittest.main()
